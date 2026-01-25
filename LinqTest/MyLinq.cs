7526422dfeb8e2659b01ac1f5f07a7d1fe884285
Ryan Luttrull <reluttrull@gmail.com>
2026-01-25 12:32:32 -0600

    cleanup

--- LinqTest/MyLinq.cs
+++ LinqTest/MyLinq.cs
@@ -5,22 +5,21 @@ namespace LinqTest
 {
     public static class MyLinq
     {
-        public static IEnumerable<TResult> Select<TSource, TResult>(
-            this IEnumerable<TSource> input,
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> collection,
             Func<TSource, TResult> transformation)
         {
             List<TResult> result = [];
-            foreach (TSource item in input)
+            foreach (TSource item in collection)
             {
                 result.Add(transformation(item));
             }
             return result;
         }
 
-        public static IEnumerable<T> Where<T>(this IEnumerable<T> input, Func<T, bool> predicate)
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
         {
             List<T> result = [];
-            foreach (T item in input)
+            foreach (T item in collection)
             {
                 if (predicate(item)) result.Add(item);
             }
@@ -32,7 +31,8 @@ namespace LinqTest
             return collection.Where(predicate).Count() > 0;
         }
 
-        public static IEnumerable<T1> OrderBy<T1, T2>(this IEnumerable<T1> collection, Func<T1, T2> prop) where T2 : IComparable<T2>
+        public static IEnumerable<T1> OrderBy<T1, T2>(this IEnumerable<T1> collection, Func<T1, T2> prop) 
+                where T2 : IComparable<T2>
         {
             Comparer<T1> comparer = Comparer<T1>.Create((x, y) => prop(x).CompareTo(prop(y)));
             var sorted = collection.ToList();
@@ -40,7 +40,8 @@ namespace LinqTest
             return sorted;
         }
 
-        public static IEnumerable<T1> OrderByDescending<T1, T2>(this IEnumerable<T1> collection, Func<T1, T2> prop) where T2 : IComparable<T2>
+        public static IEnumerable<T1> OrderByDescending<T1, T2>(this IEnumerable<T1> collection, Func<T1, T2> prop) 
+                where T2 : IComparable<T2>
         {
             Comparer<T1> comparer = Comparer<T1>.Create((x, y) => prop(y).CompareTo(prop(x)));
             var sorted = collection.ToList();
