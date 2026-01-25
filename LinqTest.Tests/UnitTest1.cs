0b2b176bc32828b0ce55e5b068a794752b15692a
Ryan Luttrull <reluttrull@gmail.com>
2026-01-25 09:30:21 -0600

    Add project files.

---
+++ LinqTest.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using LinqTest;
+
+namespace LinqTest.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Select_WhenTResultAndTSourceEqual_SuccessfullyTransforms()
+        {
+            List<int> ls = [1, 2, 3, 4, 5];
+            var select = ls.Select(x => x * 2);
+            Assert.Equal<int>(select, [2, 4, 6, 8, 10]);
+        }
+
+        [Fact]
+        public void Select_WhenDifferentTResultAndTSource_ReturnsCorrectType()
+        {
+            List<int> ls = [1, 2, 3, 4, 5];
+            var select = ls.Select(x => x.ToString());
+            Assert.IsType<string>(select.ElementAt(0));
+        }
+
+        [Fact]
+        public void Where_WhenFiltered_SuccessfullyFilters()
+        {
+            List<int> ls = [1, 2, 3, 4, 5];
+            var where = ls.Where(x => x % 2 == 0);
+            Assert.Equal(2, where.Count());
+        }
+    }
+}
---
+++ LinqTest/MyLinq.cs
@@ -0,0 +1,54 @@
+﻿namespace LinqTest
+{
+    public static class MyLinq
+    {
+        public static IEnumerable<TResult> Select<TSource, TResult>(
+            this IEnumerable<TSource> input,
+            Func<TSource, TResult> transformation)
+        {
+            List<TResult> result = [];
+            foreach (TSource item in input)
+            {
+                result.Add(transformation(item));
+            }
+            return result;
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> input, Func<T, bool> predicate)
+        {
+            List<T> result = [];
+            foreach (T item in input)
+            {
+                if (predicate(item)) result.Add(item);
+            }
+            return result;
+        }
+
+        public static bool Any<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            return collection.Where(predicate).Count() > 0;
+        }
+
+        public static T First<T>(this IEnumerable<T> collection)
+        {
+            if (collection.Count() == 0) throw new InvalidOperationException();
+            return collection.ElementAt(0);
+        }
+
+        public static T First<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            return collection.Where(predicate).First();
+        }
+
+        public static T? FirstOrDefault<T>(this IEnumerable<T> collection)
+        {
+            if (collection.Count() == 0) return default(T);
+            return collection.ElementAt(0);
+        }
+
+        public static T? FirstOrDefault<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            return collection.Where(predicate).FirstOrDefault(); 
+        }
+    }
+}
