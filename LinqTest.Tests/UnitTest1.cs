3142eb9821f9ee10b679cc0e8774615cf2b7815b
Ryan Luttrull <reluttrull@gmail.com>
2026-01-29 16:32:27 -0600

    Add basic IGrouping and GroupBy

--- LinqTest.Tests/UnitTest1.cs
+++ LinqTest.Tests/UnitTest1.cs
@@ -143,6 +143,24 @@ namespace LinqTest.Tests
         }
 
         [Fact]
+        public void GroupBy_WhenSplitIntoTwoGroups_ShouldReturnTwoGroupings()
+        {
+            List<int> ls = [0, 1, 1, 0, 1];
+            var groups = ls.GroupBy(x => x);
+            Assert.Equal(2, groups.Count());
+        }
+
+        [Fact]
+        public void GroupBy_AfterSplitIntoGroupings_ShouldBeAbleToSelectMany()
+        {
+            List<int> ls = [1, 2, 3, 4, 5];
+            var groups = ls.GroupBy(x => x % 3);
+            Assert.Equal(3, groups.Count());
+            var select = groups.SelectMany(g => g);
+            Assert.Equal(5, select.Count());
+        }
+
+        [Fact]
         public void OrderBy_WhenIComparable_SuccessfullyOrdersAsc()
         {
             List<Person> people = [new Person("Larry"), new Person("Allison"), new Person("Sam")];
---
+++ LinqTest/Group.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqTest
+{
+    public static class Group
+    {
+        public static IEnumerable<IGrouping<TKey, TElement>> GroupBy<TKey, TElement>(this IEnumerable<TElement> collection, Func<TElement, TKey> transformation)
+            where TKey : notnull
+        {
+            Dictionary<TKey, List<TElement>> dict = new();
+            foreach (TElement element in collection)
+            {
+                TKey key = transformation(element);
+                if (dict.TryGetValue(key, out List<TElement>? value))
+                {
+                    value.Add(element);
+                }
+                else
+                {
+                    dict.Add(key, [element]);
+                }
+            }
+            return dict.Select(kvp => new IGrouping<TKey, TElement>(kvp));
+        }
+        public class IGrouping<TKey, TElement>(KeyValuePair<TKey, List<TElement>> kvp) : IEnumerable<TElement>
+        {
+            public TKey Key { get; set; } = kvp.Key;
+            private List<TElement> elements = kvp.Value;
+
+            public IEnumerator<TElement> GetEnumerator()
+            {
+                foreach (TElement element in elements)
+                {
+                    yield return element;
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+}
