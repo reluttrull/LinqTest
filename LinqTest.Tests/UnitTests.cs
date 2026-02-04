0682ed5769bab5530c0561fcce22a7865590884d
Ryan Luttrull <reluttrull@gmail.com>
2026-02-04 14:26:32 -0600

    Add first Aggregate method

--- LinqTest.Tests/UnitTests.cs
+++ LinqTest.Tests/UnitTests.cs
@@ -455,6 +455,15 @@ namespace LinqTest.Tests
             Assert.Empty(distinct);
         }
 
+        [Fact]
+        public void Aggregate_WhenThreeTypes_HandlesTransformsCorrectly()
+        {
+            List<int> ls = [1, 2, 3, 4, 5, 6, 7];
+            var agg = ls.Aggregate<int, double, string>((total, next) => total += (double)next / 2, num => num.ToString());
+            Assert.IsType<string>(agg);
+            Assert.Equal("14", agg);
+        }
+
         public record Person(string Name);
         public class ChildObj : IEquatable<ChildObj>
         {
---
+++ LinqTest/Aggregate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqTest
+{
+    public static class Aggregators
+    {
+        public static TResult Aggregate<TSource, TAccumulate, TResult>(this IEnumerable<TSource> collection, 
+            Func<TAccumulate, TSource, TAccumulate> accumulator, Func<TAccumulate, TResult> finalTransform)
+            where TAccumulate : struct
+        {
+            TAccumulate accumulation = default;
+            foreach (TSource item in collection)
+            {
+                accumulation = accumulator(accumulation, item);
+            }
+            return finalTransform(accumulation);
+        }
+    }
+}
